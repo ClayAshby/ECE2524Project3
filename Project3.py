05ba2f2a1eaee47b85457dc16ce03cb42cee35b5
Clay Ashby <clay@localhost.localdomain>
2020-12-06 19:55:09 -0500

    Made a view changes to file permissions

--- Project3.py
+++ Project3.py
@@ -11,22 +11,25 @@ import os       # Required library in order to access files and directories in a
 
 # Asking the user for input
 path = input("Enter the directory you wish to begin listing files from: ")
-permissions = input("Would you like to see file permissions as well? y/n: ")
+permissions = input("Would you like to see owner file permissions as well? y/n: ")
 while permissions != "y" and permissions != "n":
-    permissions = input("Invalid input: Would you like to see file permissions as well? y/n: ")
+    permissions = input("Invalid input: Would you like to see owner file permissions as well? y/n: ")
 for root, directories, files in os.walk(path, topdown=True):
     for directory in directories:
         print(os.path.join(path, directory))
     for file in files:
-        print("\n"+file)
+        print(os.path.join(path, file))
         if permissions == "y":
-            if os.access(path+"\\"+file, os.R_OK):      # Testing if the file is readable
-                print("r", end="")
-            if os.access(path+"\\"+file, os.W_OK):      # Testing if the file is writable
+            if os.access(path+file, os.R_OK):      # Testing if the file is readable
+                print("\tr", end="")
+            else:
+                print("\t-", end="")
+            if os.access(path+file, os.W_OK):      # Testing if the file is writable
                 print("w", end="")
-            if os.access(path+"\\"+file, os.X_OK):      # Testing if the file is executable
+            else:
+                print("-", end="")
+            if os.access(path+file, os.X_OK):      # Testing if the file is executable
                 print("x", end="")
-
-'''tree = os.listdir(path)
-for name in tree:
-    print(name)'''
+            else:
+                print("-", end="")
+            print("\n")
